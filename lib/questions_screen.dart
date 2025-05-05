4ea1aeaa467658ed38734d54ab0b0703ad0c368c
Tschongsti <lukas.angster@gmail.com>
2025-05-05 19:40:49 +0200

    Stand nach Kapitel 72 des Udemy-Kurses

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen ({super.key});
 
@@ -13,6 +16,28 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
   @override
   Widget build (context) {
-    return Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Text(
+          currentQuestion.text,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 30),
+        ...currentQuestion.answers.map((answer) {
+          return AnswerButton(
+            answer,
+            (){},
+            );
+        })
+        ],
+      ),
+    );
   }
 }
